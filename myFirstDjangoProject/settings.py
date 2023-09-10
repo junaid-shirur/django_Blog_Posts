2cb8163b235e8d2533fd05e9d05d82c3a96408c4
Junaid <jshirur786@gmail.com>
2023-09-10 16:09:17 +0530

    user model, views, and url added

--- myFirstDjangoProject/settings.py
+++ myFirstDjangoProject/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    "rest_framework",
 
     #apps
-    "projects.apps.ProjectsConfig"
+    "projects.apps.ProjectsConfig",
+    "users.apps.UsersConfig"
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+AUTH_USER_MODEL = "users.User"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
