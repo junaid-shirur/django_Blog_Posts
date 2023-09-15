6aa0f1bb2b0c2838cb0c9a06dce4b6cd0f7551ac
Junaid <jshirur786@gmail.com>
2023-09-16 00:41:26 +0530

    service model

--- virtual_queue/models.py
+++ virtual_queue/models.py
@@ -2,3 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+# class Service_Details(models.Model):
+#     name = models.CharField(max_length=100)
+#     def __str__(self):
+#         return self.name
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    operating_hours = models.CharField(max_length=100)
+    estimated_wait_time = models.PositiveIntegerField()
+    service_details = models.TextField()
+
+    def __str__(self):
+        return self.name
