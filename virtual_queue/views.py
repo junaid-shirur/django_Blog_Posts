d8ee3d418bb15cf80742ebd542bf49ee9279fe98
Junaid <jshirur786@gmail.com>
2023-09-19 23:59:43 +0530

    services api route added

--- virtual_queue/views.py
+++ virtual_queue/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializer import UserSerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from .models import User
+from django.contrib.auth import authenticate,login as django_login
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from users.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 # Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getServices(request):
+    pass
