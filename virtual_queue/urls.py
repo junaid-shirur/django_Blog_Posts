fd1d42d78a794889e804c007f6f6d2cd974c1b11
Junaid <jshirur786@gmail.com>
2023-09-23 11:09:46 +0530

    services view

--- virtual_queue/urls.py
+++ virtual_queue/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-# from .views import 
+from .views import getServices
 
 
 urlpatterns = [
-    path("services"),
+    path("services", getServices),
 ]
--- virtual_queue/views.py
+++ virtual_queue/views.py
@@ -1,18 +1,17 @@
-from rest_framework.views import APIView
-from .serializer import UserSerializer
+from .serializer import ServiceSerializer
 from rest_framework.response import Response
-from rest_framework.exceptions import AuthenticationFailed
-from .models import User
-from django.contrib.auth import authenticate,login as django_login
+from .models import User,Services
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from users.models import User
-from rest_framework_simplejwt.tokens import RefreshToken
-
 
 # Create your views here.
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getServices(request):
-    pass
+    user = request.user
+    if not user :
+       return Response("Please login", status=status.HTTP_400_BAD_REQUEST)
+    services = Services.objects.all()
+    serializer = ServiceSerializer(services, many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
