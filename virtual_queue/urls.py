22fda11162a2a422587b4001c3f1cf7e09144584
Junaid <jshirur786@gmail.com>
2023-11-26 12:09:22 +0530

    join queue view added in urls

--- virtual_queue/urls.py
+++ virtual_queue/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import getServices, createService
+from .views import getServices, createService,joinQueue
 
 
 urlpatterns = [
     path("services", getServices),
     path("create-service", createService),
+    path("join_queue", joinQueue)
 ]
