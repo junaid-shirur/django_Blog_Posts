8af8389e985e450836b67196defb2c07d3fe857c
Junaid <jshirur786@gmail.com>
2023-11-26 12:14:54 +0530

    join queue view WIP

--- virtual_queue/views.py
+++ virtual_queue/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
 # Create your views here.
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -35,13 +36,41 @@ def createService(request):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def joinQueue(request):
+    service_id = request.data.get('service_id')
 
-    service = get_object_or_404(Services, id=request.service_id)
-    queue = Queue.objects.filter(service=service, queue_status='open', current_queue_size__lt=20).first()
-    if not queue :
-       return Response(f'currently queue is not available for the service {service if service.name else ""}', status=status.HTTP_400_BAD_REQUEST)
+    if not service_id:
+        return Response("Please provide a service_id in the request data", status=status.HTTP_400_BAD_REQUEST)
+
+    service = get_object_or_404(Services, id=service_id)
     
+    # Try to get an existing open queue or create a new one if not available
+    queue, created = Queue.objects.get_or_create(
+        service=service,
+        queue_status='open',
+        defaults={'max_capacity': 20, 'current_wait_time': timezone.now(), 'estimated_wait_time': 0}
+    )
+    if created:
+        # If the queue was created, increment current_queue_size
+        queue.current_queue_size += 1
+        queue.save()
+
+        if queue.current_queue_size >= queue.max_capacity:
+            return Response(f'The queue for the service {service.name if service.name else ""} is currently full', status=status.HTTP_400_BAD_REQUEST)
+    elif queue:
+        # If the queue already exists, increment current_queue_size
+        queue.current_queue_size += 1
+        queue.save()
+    else:
+        return Response(f'The queue for the service {service.name if service.name else ""} does not exist', status=status.HTTP_400_BAD_REQUEST)
+
+  
     user_participation = QueueParticipant.objects.filter(participant=request.user, queue=queue).first()
 
-    if user_participation :
-        return Response("user already in queue, wait your turn comes", status=status.HTTP_400_BAD_REQUEST)
+    if user_participation:
+        return Response("User is already in the queue, wait your turn to come", status=status.HTTP_400_BAD_REQUEST)
+
+    # Add your logic for joining the queue here, e.g., creating a QueueParticipant entry
+    QueueParticipant.objects.create(participant=request.user, queue=queue, status='pending')
+
+    return Response("Successfully joined the queue", status=status.HTTP_200_OK)
+
