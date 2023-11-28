a5bb31f67584e63bc4041de7a5d846e3e1b67701
Junaid <jshirur786@gmail.com>
2023-11-28 10:00:52 +0530

    update current queue size when joined

--- virtual_queue/views.py
+++ virtual_queue/views.py
@@ -50,39 +50,36 @@ def joinQueue(request):
     try:
         with transaction.atomic():
             # Try to get an existing open queue or create a new one if not available
-            queue = Queue.objects.filter(
+            queue, created = Queue.objects.get_or_create(
                 service=service,
                 queue_status='open',
-                current_queue_size__lt=F('max_capacity')
-            ).select_for_update().first()
+                defaults={
+                    'max_capacity': 20,
+                    'current_wait_time': timezone.now(),
+                    'estimated_wait_time': 0,
+                    'current_queue_size': 0,
+                }
+            )
 
-            if not queue:
-                # If the queue doesn't exist, create a new one
-                queue = Queue.objects.create(
-                    service=service,
-                    queue_status='open',
-                    max_capacity=20,
-                )
-
-            if queue.current_queue_size >= queue.max_capacity:
+            if not created and queue.current_queue_size >= queue.max_capacity:
                 return Response(f'The queue for the service {service.name if service.name else ""} is currently full', status=status.HTTP_400_BAD_REQUEST)
 
             user_participation = QueueParticipant.objects.filter(participant=request.user, queue=queue).first()
 
             if user_participation:
-                return Response("You've already join the queue, wait your turn to come", status=status.HTTP_400_BAD_REQUEST)
+                return Response("You've already joined the queue, wait for your turn to come", status=status.HTTP_400_BAD_REQUEST)
 
-            # Add your logic for joining the queue here, e.g., creating a QueueParticipant entry
-            QueueParticipant.objects.create(participant=request.user, queue=queue, status='pending')
-            # Increment current_queue_size atomically
+            # Increment current_queue_size and update estimated_wait_time atomically
             queue.current_queue_size += 1
             queue.current_wait_time += timedelta(minutes=5)
-            queue.estimated_wait_time = queue.current_queue_size - 1
+            queue.estimated_wait_time = queue.current_queue_size - 1 if queue.current_queue_size >= 2 else 1
             queue.save()
+
+            # Add your logic for joining the queue here, e.g., creating a QueueParticipant entry
+            QueueParticipant.objects.create(participant=request.user, queue=queue, status='in_progress')
+
             return Response("Successfully joined the queue", status=status.HTTP_200_OK)
 
     except Exception as e:
         # Handle any exceptions that may occur during the transaction
         return Response(f"An error occurred: {str(e)}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
