e76ebc914c6da96c2656492cb7dc66ee763dd4ec
Junaid <jshirur786@gmail.com>
2023-12-05 11:10:07 +0530

    queue,slot serializers

--- virtual_queue/serializer.py
+++ virtual_queue/serializer.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Services
+from .models import Services,Slot, Queue
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Services
         fields = '__all__'
+
+
+class SlotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Slot
+        fields = ['id', 'start_time', 'end_time', 'capacity']
+
+class QueueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Queue
+        fields = ['id', 'slot', 'date', 'user']
 
